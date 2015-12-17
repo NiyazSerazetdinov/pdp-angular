f96af58f32a177edd2beeaf5907c25bc81f62e32
Niyaz Serazetdinov <niyaz.serazetdinov@flatstack.com>
2015-12-17 04:27:34 +0300

    clean up

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular
+  .module('PdpAngular', ['ngRoute'])
+  .config(['$routeProvider', RouteProvider]);
+
+function RouteProvider($routeProvider) {
+  $routeProvider
+    .when('/articles', {
+      templateUrl: 'articles/index.html',
+      controller: 'ArticlesController'
+    })
+    .otherwise({
+      redirectTo: '/articles'
+    });
+};
