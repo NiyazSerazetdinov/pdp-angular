1b5facd66fd264b14528a0d4c77467547a23793e
NiyazSerazetdinov <niyaz.serazetdinov@flatstack.com>
2015-12-17 15:23:28 +0300

    add comments

--- app/articles/articlesShow.controller.js
+++ app/articles/articlesShow.controller.js
@@ -5,9 +5,19 @@ angular
   .controller('ArticlesShowController', ['$scope', '$http','$routeParams', ArticlesShowController])
 
 function ArticlesShowController($scope, $http, $routeParams) {
-  $http.get('http://pdp-angular-api.herokuapp.com/v1/articles/'+$routeParams.articleId)
+  var articleUrl = 'http://pdp-angular-api.herokuapp.com/v1/articles/'+$routeParams.articleId;
+
+  $http.get(articleUrl)
     .then(function(response) {
-      console.log(response.data);
       $scope.article = response.data.article;
+      $scope.comments = response.data.article.comments;
     });
+
+  $scope.postComment = function(comment) {
+    $http.post(articleUrl+'/comments', comment)
+      .then(function(response) {
+        $scope.comments.push(response.data.comment);
+        $scope.comment.text = "";
+      })
+  };
 }
